33fd16eb4e23205d059d6e22e5f2c08c555fde97
Ophélia <ophelia@MacBook-Pro.local>
2024-10-08 09:51:05 +0200

    first commit

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,58 +1,62 @@
-import React from 'react'
-import {faGithub, faYoutube, faLinkedin} from '@fortawesome/free-brands-svg-icons';
-import { FontAwesomeIcon, file } from '@fortawesome/react-fontawesome';
-import { faFilePdf } from '@fortawesome/free-solid-svg-icons'
-import {Link} from 'react-router-dom';
-import CV from '../docs/CV_OpheliaTagbo_DevWebAlt.pdf'
+import React from "react"
+import {
+  faGithub,
+  faYoutube,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon, file } from "@fortawesome/react-fontawesome"
+import { faFilePdf } from "@fortawesome/free-solid-svg-icons"
+import { Link } from "react-router-dom"
+import CV from "../docs/CV_OpheliaTagbo_DevWebAlt.pdf"
 
 function HomePage() {
-    return (
-        <div className="HomePage">
-            <header className="hero">
-            <h1 className="hero-text">
-                Bonjour,   je m'appelle
-                <span> Ophélia </span>
-            </h1>
-                <p className="h-sub-text">
-                    <br/>
-                    <strong>
-                    <i>Développeuse passionnée </i>
-                    </strong>
-                    je code depuis presque deux ans, d'abord en solo
-                    puis en me formant au sein de 
-                    <strong>
-                    <i> l'Ecole de Nantes Informatique </i>
-                    </strong> 
-                    et de
-                    <strong>
-                    <i> La Capsule </i> 
-                    </strong>
-                    <br/>
-                    <br/> Aujourd'hui j'aimerai m'investir dans un projet, trouver <strong>une entreprise mais surtout une équipe </strong> au service de qui mettre mes compétences et surtout <br/>
-                    <strong>continuer, encore et toujours à apprendre!</strong>
-
-
-
-                </p>
-                <div className="icons">
-                    <a href="https://www.linkedin.com/in/ophélia-tagbo" className="icon-holder">
-                        <FontAwesomeIcon icon={faLinkedin} className="icon lkdin" />
-                    </a>
-                    <a href="https://github.com/Ophe69" className="icon-holder">
-                        <FontAwesomeIcon icon={faGithub} className="icon gh" />
-                    </a>
-                    <a href={CV} className="icon-holder">
-                        <FontAwesomeIcon icon={faFilePdf} className="icon cv" />
-                    </a>
-                    {/* <Link to="test" className="icon-holder">
+  return (
+    <div className="HomePage">
+      <header className="hero">
+        <h1 className="hero-text">
+          Bonjour, je m'appelle
+          <span> Ophélia </span>
+        </h1>
+        <p className="h-sub-text">
+          <br />
+          <strong>
+            <i>Développeuse passionnée </i>
+          </strong>
+          je code depuis presque deux ans, d'abord en solo puis en me formant au
+          sein de
+          <strong>
+            <i> l'Ecole de Nantes Informatique </i>
+          </strong>
+          et de
+          <strong>
+            <i> La Capsule </i>
+          </strong>
+          <br />
+          <br /> Aujourd'hui j'aimerai m'investir dans un projet, trouver{" "}
+          <strong>une entreprise mais surtout une équipe </strong> au service de
+          qui mettre mes compétences et surtout <br />
+          <strong>continuer, encore et toujours à apprendre!</strong>
+        </p>
+        <div className="icons">
+          <a
+            href="https://www.linkedin.com/in/ophélia-tagbo"
+            className="icon-holder"
+          >
+            <FontAwesomeIcon icon={faLinkedin} className="icon lkdin" />
+          </a>
+          <a href="https://github.com/Ophe69" className="icon-holder">
+            <FontAwesomeIcon icon={faGithub} className="icon gh" />
+          </a>
+          <a href={CV} className="icon-holder">
+            <FontAwesomeIcon icon={faFilePdf} className="icon cv" />
+          </a>
+          {/* <Link to="test" className="icon-holder">
                         <FontAwesomeIcon icon={faYoutube} className="icon yt"/>
                     </Link> */}
-                </div>
-                
-
-            </header>
         </div>
-    )
+      </header>
+    </div>
+  )
 }
 
-export default HomePage;
+export default HomePage
